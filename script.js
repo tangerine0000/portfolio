56f683590f6bae67b3408fe391f71eee3c9be221
nttan <tan.personal.mail@gmail.com>
2025-05-21 08:08:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId)
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+
+            if (history.pushState) {
+                history.pushState(null, null, targetId);
+            } else {
+                location.hash = targetId;
+            }
+        }
+    })
+})
